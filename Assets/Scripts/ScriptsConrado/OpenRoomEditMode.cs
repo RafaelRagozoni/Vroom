036e00d787d74c5d58c506ade5442febfbd07114
RafaelRagozoni <rafael.r.conrado11@hotmail.com>
2025-05-19 12:49:55 -0300

    room scripts

--- Assets/Scripts/ScriptsConrado/OpenRoomEditMode.cs
+++ Assets/Scripts/ScriptsConrado/OpenRoomEditMode.cs
@@ -7,8 +7,17 @@ public class OpenRoomEditMode : MonoBehaviour
 {
     public GameObject sceneCamera;
     public bool dentro = true;
-
     private bool canRun = true;
+    public RayInteractor rayInteractorRight;
+    public RayInteractor rayInteractorLeft;
+
+    public Transform oculusCursorRight;
+    public Transform oculusCursorLeft;
+    public Transform selectionCircleRight;
+    public Transform selectionCircleLeft;
+
+    public RoomReshaper roomReshaper;
+
 
     public void OpenRoomEditModeFunction()
     {
@@ -18,14 +27,27 @@ public class OpenRoomEditMode : MonoBehaviour
         // Faz a camera fica na posição de cima 
         if (dentro)
         {
-            sceneCamera.transform.position = new Vector3(25, 35, -25);
-            sceneCamera.transform.rotation = Quaternion.Euler(45, -45, 0);
+            sceneCamera.transform.position = new Vector3(-10, 35, -10);
+            rayInteractorRight.MaxRayLength = 100;
+            rayInteractorLeft.MaxRayLength = 100;
+            oculusCursorRight.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            oculusCursorLeft.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            selectionCircleRight.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            selectionCircleLeft.transform.localScale = new Vector3(0.5f, 0.5f, 1f);
+            roomReshaper.enabled = true;
             dentro = false;
         }
         else
         {
             sceneCamera.transform.position = new Vector3(0, 0, 0);
             sceneCamera.transform.rotation = Quaternion.Euler(0, 0, 0);
+            rayInteractorRight.MaxRayLength = 5;
+            rayInteractorLeft.MaxRayLength = 5;
+            oculusCursorRight.transform.localScale = new Vector3(0.1f, 0.1f, 1f);
+            oculusCursorLeft.transform.localScale = new Vector3(0.1f, 0.1f, 1f);
+            selectionCircleRight.transform.localScale = new Vector3(0.016f, 0.016f, 1f);
+            selectionCircleLeft.transform.localScale = new Vector3(0.016f, 0.016f, 1f);
+            roomReshaper.enabled = false;
             dentro = true;
         }
 
---
+++ Assets/Scripts/ScriptsConrado/RoomReshaper.cs
@@ -0,0 +1,121 @@
+using Meta.Voice;
+using UnityEngine;
+
+public class RoomReshaper : MonoBehaviour
+{
+    public GameObject wallR;
+    public GameObject wallL;
+    public GameObject wallB;
+    public GameObject wallF;
+    public GameObject celling;
+    public GameObject floor;
+    public GameObject gizmoX;
+    public GameObject gizmoY;
+    public GameObject gizmoZ;
+    private float originalGizmoX;
+    private float originalGizmoY;
+    private float originalGizmoZ;
+
+    public void Start()
+    {
+        originalGizmoX = gizmoX.transform.position.x;
+        originalGizmoY = gizmoY.transform.position.y;
+        originalGizmoZ = gizmoZ.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Remodelate();
+    }
+
+    void Remodelate()
+    {
+        float deltaGizmoY = gizmoY.transform.position.y - originalGizmoY;
+        float deltaGizmoX = gizmoX.transform.position.x - originalGizmoX;
+        float deltaGizmoZ = gizmoZ.transform.position.z - originalGizmoZ;
+
+        // puxando o Y
+        wallB.transform.position = new Vector3(
+            wallB.transform.position.x + deltaGizmoX / 2,
+            wallB.transform.position.y + deltaGizmoY / 2,
+            wallB.transform.position.z
+        );
+        wallF.transform.position = new Vector3(
+            wallF.transform.position.x + deltaGizmoX / 2,
+            wallF.transform.position.y + deltaGizmoY / 2,
+            wallF.transform.position.z + deltaGizmoZ
+        );
+        wallL.transform.position = new Vector3(
+            wallL.transform.position.x,
+            wallL.transform.position.y + deltaGizmoY / 2,
+            wallL.transform.position.z + deltaGizmoZ / 2
+        );
+        wallR.transform.position = new Vector3(
+            wallR.transform.position.x + deltaGizmoX,
+            wallR.transform.position.y + deltaGizmoY / 2,
+            wallR.transform.position.z + deltaGizmoZ / 2
+        );
+        celling.transform.position = new Vector3(
+            celling.transform.position.x + deltaGizmoX / 2,
+            celling.transform.position.y + deltaGizmoY,
+            celling.transform.position.z + deltaGizmoZ / 2
+        );
+        floor.transform.position = new Vector3(
+            floor.transform.position.x + deltaGizmoX / 2,
+            floor.transform.position.y,
+            floor.transform.position.z + deltaGizmoZ / 2
+        );
+        gizmoX.transform.position = new Vector3(
+            gizmoX.transform.position.x,
+            gizmoX.transform.position.y + deltaGizmoY / 2,
+            gizmoX.transform.position.z + deltaGizmoZ / 2
+        );
+        gizmoZ.transform.position = new Vector3(
+            gizmoZ.transform.position.x + deltaGizmoX / 2,
+            gizmoZ.transform.position.y + deltaGizmoY / 2,
+            gizmoZ.transform.position.z
+        );
+        gizmoY.transform.position = new Vector3(
+            gizmoY.transform.position.x + deltaGizmoX / 2,
+            gizmoY.transform.position.y,
+            gizmoY.transform.position.z + deltaGizmoZ / 2
+        );
+
+        wallB.transform.localScale = new Vector3(
+            wallB.transform.localScale.x + deltaGizmoX / 10,
+            wallB.transform.localScale.y + deltaGizmoY / 10,
+            wallB.transform.localScale.z + deltaGizmoY / 10
+        );
+        wallF.transform.localScale = new Vector3(
+            wallF.transform.localScale.x + deltaGizmoX / 10,
+            wallF.transform.localScale.y + deltaGizmoY / 10,
+            wallF.transform.localScale.z + deltaGizmoY / 10
+        );
+        wallL.transform.localScale = new Vector3(
+            wallL.transform.localScale.x + deltaGizmoZ / 10,
+            wallL.transform.localScale.y + deltaGizmoY / 10,
+            wallL.transform.localScale.z + deltaGizmoY / 10
+        );
+        wallR.transform.localScale = new Vector3(
+            wallR.transform.localScale.x + deltaGizmoZ / 10,
+            wallR.transform.localScale.y + deltaGizmoY / 10,
+            wallR.transform.localScale.z + deltaGizmoY / 10
+        );
+        celling.transform.localScale = new Vector3(
+            celling.transform.localScale.x + deltaGizmoX / 10,
+            celling.transform.localScale.y + deltaGizmoZ / 10,
+            celling.transform.localScale.z + deltaGizmoZ / 10
+        );
+        floor.transform.localScale = new Vector3(
+            floor.transform.localScale.x + deltaGizmoX / 10,
+            floor.transform.localScale.y + deltaGizmoZ / 10,
+            floor.transform.localScale.z + deltaGizmoZ / 10
+        );
+        originalGizmoX = gizmoX.transform.position.x;
+        originalGizmoY = gizmoY.transform.position.y;
+        originalGizmoZ = gizmoZ.transform.position.z;
+    }
+
+
+}
